296bcf675bdbf19e3fade87e99a1bb07e39340a1
ed <ed@robuntu.(none)>
2014-08-28 18:54:40 +0100

    first commit

---
+++ puppet.py
@@ -0,0 +1,275 @@
+#!/usr/bin/python
+from __future__ import division
+
+import rospy
+import curses
+import pickle
+import dynamic_reconfigure.client
+from sr_ronex_msgs.msg import PWM, GeneralIOState
+from std_msgs.msg import Bool
+from std_msgs.msg import Float32
+from math import *
+from collections import deque
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.pyplot import plot, ion, show
+from matplotlib.mlab import *
+from numpy.random import uniform, seed
+
+from pylab import *
+
+ronex_id = "1403017360"
+ronex_path = "/ronex/general_io/" + ronex_id + "/"
+
+startTime=-1;
+
+pub0 = rospy.Publisher(ronex_path+"command/pwm/0", PWM)
+pub1 = rospy.Publisher(ronex_path+"command/pwm/1", PWM)
+pub2 = rospy.Publisher(ronex_path+"command/pwm/2", PWM)
+pub3 = rospy.Publisher(ronex_path+"command/pwm/3", PWM)
+pub4 = rospy.Publisher(ronex_path+"command/pwm/4", PWM)
+pub5 = rospy.Publisher(ronex_path+"command/pwm/5", PWM)
+
+pwms = [pub0,pub1,pub2,pub3,pub4,pub5]
+
+online=False #Set to false to run Particle Swarm Optimization demo without robot
+
+if online:
+    stdscr = curses.initscr()
+
+active = False 
+supressed=True
+
+analogue_input_queue = deque([],4) 
+
+pub_output = rospy.Publisher('puppet', Float32)
+
+
+#Particle Swarm Optomization variables
+w = 0.729844 # Inertia weight to prevent velocities becoming too large
+c1 = 1.496180 # Scaling co-efficient on the social component
+c2 = 1.496180 # Scaling co-efficient on the cognitive component
+dimension = 2 # Size of the problem
+particle_swarm_size = 20
+particle_positions = np.random.rand(particle_swarm_size,dimension)
+for index in range(particle_swarm_size):
+    for i in range(dimension):
+        particle_positions[index,i]=np.random.uniform(-1,1)
+particle_values = [np.nan]*particle_swarm_size
+particle_velocities = np.zeros_like(particle_positions)
+particle_maxima_positions=np.zeros_like(particle_positions)
+particle_maxima_positions[:]=np.nan
+particle_minima_positions=np.zeros_like(particle_positions)
+particle_minima_positions[:]=np.nan
+particle_maxima_values=[-np.inf]*particle_swarm_size
+particle_minima_values=[np.inf]*particle_swarm_size
+particle_global_maxima_position=[np.nan]*dimension
+particle_global_minima_position=[np.nan]*dimension
+particle_global_maxima_value=-np.inf
+particle_global_minima_value=np.inf
+
+def func(p) : #Trivial function for testing Particle Swarm Omptomization
+    z=0.0
+    for n in p:
+        for i in range(1,12) :
+            z=z+(sin(n*(6.0/i))*(1.0/i))
+    return z
+
+#precompute a mesh of points for visualising trivial PSO function
+xvec = np.linspace(-2.,2.,100)                               
+x_map,y_map = np.meshgrid(xvec, xvec)
+z_map = func([x_map,y_map])
+
+frame_counter=0
+
+def subscriber_cb(msg) : #called whenever RoNeX updates (ie: every frame)
+    global active, supressed
+    
+    if online:
+        c = stdscr.getch() #get keypresses from console 
+        if c != curses.ERR:
+            if c == ord(' ') : #space bar toggles all servos on/off
+                active = not active
+                configure_servos(active)
+            elif c == ord('z') : #z toggles wave behaviour on/off
+                supressed = not supressed
+    
+    #------------------------
+    
+    analogue_inputs = get_servo_positions(msg)
+
+    analogue_input_queue.append(analogue_inputs)
+    
+    if len(analogue_input_queue)>3:
+        a1 = []
+        a2 = []
+        for n in range(12):
+            a1.append((analogue_input_queue[0][n]+analogue_input_queue[1][n])/2)
+            a2.append((analogue_input_queue[2][n]+analogue_input_queue[3][n])/2)
+        d = 0.0
+        for n in range(12):
+            d=d+(a1[n]-a2[n])**2
+        d = sqrt(d)
+        pub_output.publish(d)
+        #rospy.loginfo("analogue_input_queue.length = %d", len(analogue_input_queue))
+        
+        output_positions = map(make_waves, range(1, 13)) #get an array of desired servo positions
+        
+        if not online: #PSO test starts here
+            global particle_velocities, particle_positions, particle_swarm_size, particle_global_maxima_position, particle_global_maxima_value, particle_global_minima_position, particle_global_minima_value
+            
+            for index in range(particle_swarm_size): #update velocities
+                for i in range(dimension):
+                    r1 = np.random.uniform()
+                    r2 = np.random.uniform()
+                    social=0.0
+                    cognitive=0.0
+                    if index%2==0: #Even indeces seek maxima, odd indeces seek minima
+                        if not np.isnan(particle_global_maxima_position[i]) :
+                            social = c1 * r1 * (particle_global_maxima_position[i] - particle_positions[index,i])
+                        if not np.isnan(particle_maxima_positions[index,i]) :
+                            cognitive = c2 * r2 * (particle_maxima_positions[index,i] - particle_positions[index,i])  
+                    else:
+                        if not np.isnan(particle_global_minima_position[i]) :
+                            social = c1 * r1 * (particle_global_minima_position[i] - particle_positions[index,i])
+                        if not np.isnan(particle_minima_positions[index,i]) :
+                            cognitive = c2 * r2 * (particle_minima_positions[index,i] - particle_positions[index,i]) 
+                             
+                    particle_velocities[index,i] = (w * particle_velocities[index,i]) + social + cognitive          
+            
+            for index in range(particle_swarm_size): #update cognitive and social maxima and minima
+                
+                particle_values[index]=func(particle_positions[index,:])
+                
+                if particle_values[index] > particle_global_maxima_value:
+                    particle_global_maxima_value=particle_values[index]
+                    for i in range(dimension): 
+                        particle_global_maxima_position[i]=particle_positions[index,i]
+                if particle_values[index] > particle_maxima_values[index]:
+                    particle_maxima_values[index]=particle_values[index]
+                    for i in range(dimension):                    
+                        particle_maxima_positions[index,i]=particle_positions[index,i]
+                
+                if particle_values[index] < particle_global_minima_value:
+                    particle_global_minima_value=particle_values[index]
+                    for i in range(dimension): 
+                        particle_global_minima_position[i]=particle_positions[index,i]
+                if particle_values[index] < particle_minima_values[index]:
+                    particle_minima_values[index]=particle_values[index]
+                    for i in range(dimension):                    
+                        particle_minima_positions[index,i]=particle_positions[index,i]
+                
+                
+            particle_positions=np.add(particle_positions,particle_velocities)   #add velocities to positions
+            
+            plt.clf()
+            #plt.subplot(211) 
+            plt.contour(x_map, y_map, z_map, alpha=0.5)
+            
+            #Even indeces = [::2,0] = seek maxima, odd indeces = [1::2,0] = seek minima  
+            plt.scatter(particle_positions[::2,0],particle_positions[::2,1],c='red',marker='+', alpha=0.5, label='maxima seeking particles')
+            plt.scatter(particle_positions[1::2,0],particle_positions[1::2,1],c='blue',marker='x', alpha=0.5, label='minima seeking particles')
+            
+            plt.xlim(-2, 2)
+            plt.ylim(-2, 2)
+            #plt.axis('off')
+            plt.legend(loc='best')
+            #plt.subplot(212) 
+            #plt.plot(particle_values)  
+            plt.draw()
+            #global frame_counter
+            #plt.savefig("puppetfig"+str(frame_counter).zfill(3))
+            #frame_counter=frame_counter+1
+    
+    #INSERT INTERESTING BIT HERE    
+    
+    #------------------------
+        
+    if active:
+        set_servo_angles(output_positions)
+    #else: 
+    #    startTime=rospy.get_rostime()
+
+
+def get_servo_positions(msg) : #get analogue value from an individual servo
+    analogue_inputs = []
+    for index in range(12): #fill an array with the analogue servo inputs
+        analogue_inputs.append(msg.analogue[index]) # range 0 -- 3690
+    return analogue_inputs 
+
+
+def make_waves(index) : #generates out-of-sync sine waves for each servo
+    if supressed :
+        return 0.5
+    now = rospy.get_rostime()
+    time = now.to_sec()-startTime.to_sec()
+    minus_one_to_one = sin (time*(1+index/6.0))
+    minus_one_to_one/=10;
+    zero_to_one = (minus_one_to_one+1.0)/2.0
+    return  zero_to_one
+
+
+def angle_zero_to_one_to_pwm(angle_zero_to_one) :
+    return  1600+ angle_zero_to_one  * 4800
+
+
+def set_servo_angle(angle_zero_to_one,servo_index) : #set an individual servo position
+    pwm_message = PWM()
+    pwm_message.pwm_period = 64000
+    pwm_module=servo_index/2; #RoNeX has six PWM modules each driving two servos
+    if servo_index % 2 == 0 :
+        pwm_message.pwm_on_time_0 =  angle_zero_to_one_to_pwm(angle_zero_to_one)
+    else :
+        pwm_message.pwm_on_time_1 =  angle_zero_to_one_to_pwm(angle_zero_to_one)
+    
+
+def set_servo_angles(angles_zero_to_one) : #set all the servo from a list of positions
+    pwm_message = PWM()
+    pwm_message.pwm_period = 64000
+    for index, pwm in enumerate(pwms) : #RoNeX has six PWM modules each driving two servos 
+        pwm_message.pwm_on_time_0 =  angle_zero_to_one_to_pwm(angles_zero_to_one[index*2])
+        pwm_message.pwm_on_time_1 =  angle_zero_to_one_to_pwm(angles_zero_to_one[(index*2)+1])
+        pwm.publish(pwm_message)
+
+  
+def shutdown(): #put the console back to normal and turn the servos off
+    global online    
+    configure_servos(False)
+    if online:
+        curses.nocbreak()
+        stdscr.keypad(0)
+        curses.echo()
+        curses.endwin()
+        plt.close('all')
+    
+
+def configure_servos(on): #turn all servos on or off
+    client = dynamic_reconfigure.client.Client(ronex_path)
+    params = { 'input_mode_0' : not on, 'input_mode_1' : not on, 'input_mode_2' : not on,'input_mode_3' : not on,'input_mode_4' : not on,'input_mode_5' : not on,'input_mode_6' : not on,'input_mode_7' : not on,'input_mode_8' : not on,'input_mode_9' : not on,'input_mode_10' : not on,'input_mode_11' : not on,}
+    config = client.update_configuration(params)
+    
+
+
+def setup_visualisation():
+    plt.ion()
+    plt.show()
+    
+
+if __name__ == "__main__": #setup
+    setup_visualisation()
+    
+    rospy.init_node("change_ronex_configuration_py")
+    rospy.on_shutdown(shutdown)
+    configure_servos(active)
+    startTime=rospy.get_rostime();
+    rospy.Subscriber(ronex_path+"state", GeneralIOState, subscriber_cb)
+    if online:
+        stdscr.clear()
+        stdscr.nodelay(1)
+        stdscr.addstr("Hello RoNeX\n")
+        curses.noecho() #make the terminal accept individual key-presses and not echo them to the screen
+        
+    
+    rospy.spin()
